75e4b58bb9be2254ebe731ae809b2acb67d58825
Lorenzo Caballero <zeldiuss@gmail.com>
2023-11-29 12:03:40 -0300

    git ignore

--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,7 @@
 // api.ts
 import axios from 'axios';
 
-const apiUrl = 'https://api.openai.com/v1/chat/completions';  
+const apiUrl = 'https://api.openai.com/v1/chat/completions';
 
 export const obtenerRespuestaAoki = async (userMessage: string) => {
   try {
@@ -9,17 +9,20 @@ export const obtenerRespuestaAoki = async (userMessage: string) => {
       apiUrl,
       {
         messages: [
-          {"role": "system", "content": "Eres un asistente de Aoki ,como asistente virtual de aoki , debes conocer que en Aoki es una empresa de desarrollo , Nacio en 2011 como departamento de sistemas de una empresa mayorista de autopartes. En 2022 se convirtio en una organización independiente y ganamos el Premio Argentina Tecnológica. debes responder con amabilidad preguntas referenciadas a la tecnologia si estan fuera del contexto tecnologico responde con un No conozco esos temas! (:, ademas ten en cuenta que tienes un limite de respuesta de 70 tokens , evita que tu respuesta supere esos tokens"},
-          {"role": "user", "content": userMessage}
+          {
+            role: 'system',
+            content:
+              'Eres un asistente de Aoki, como asistente virtual de Aoki, debes conocer que en Aoki es una empresa de desarrollo. Nació en 2011 como departamento de sistemas de una empresa mayorista de autopartes. En 2022 se convirtió en una organización independiente y ganamos el Premio Argentina Tecnológica. Debes responder con amabilidad preguntas referenciadas a la tecnología. Si están fuera del contexto tecnológico, responde con un "No conozco esos temas! (: Además, ten en cuenta que tienes un límite de respuesta de 70 tokens. Evita que tu respuesta supere esos tokens.',
+          },
+          { role: 'user', content: userMessage },
         ],
-        model: "gpt-3.5-turbo",
+        model: 'gpt-3.5-turbo',
         max_tokens: 70,
-
       },
       {
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer 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`,
+          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
         },
       }
     );
